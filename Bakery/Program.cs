cbb9dee0d384afb360bd61468da5df59d09766d5
Aidan Williams <aidanwilliams01@gmail.com>
2023-09-29 16:29:22 -0700

    add initial UI logic

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -2,15 +2,23 @@ using System;
 using Bakery.Models;
 using System.Collections.Generic;
 
-namespace ScorerNamespace
+namespace Bakery
 {
   class Program
   {
     static void Main()
     {
-      // Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
-      // Console.WriteLine("Welcome to the Scrabble Scorer app!");
-      // Console.WriteLine("Enter a word:");
+      Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
+      Console.WriteLine("Pierre's Bakery");
+      Console.WriteLine("How many loaves of bread?");
+      int loaves = int.Parse(Console.ReadLine());
+      Console.WriteLine("How many pastrys?");
+      int pastrys = int.Parse(Console.ReadLine());
+      // add edge-case handling
+      Bread bread = new Bread(loaves);
+      Pastry pastry = new Pastry(pastrys);
+      float cost = bread.CalculateCost() + pastry.CalculateCost();
+      Console.WriteLine($"The total cost is ${cost}.");
     }
   }
 }
